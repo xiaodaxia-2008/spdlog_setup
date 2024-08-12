4ee64a783125984a68d29dec75e106e97f6195b8
xiaodaxia <xiaozisheng2008@qq.com>
2024-08-12 22:12:49 +0800

    fix conflicts with std::format

--- include/spdlog_setup/details/template_impl.h
+++ include/spdlog_setup/details/template_impl.h
@@ -36,9 +36,6 @@ inline auto is_valid_var_char(const char c) -> bool {
 inline auto render(
     const std::string &tmpl,
     const std::unordered_map<std::string, std::string> &m) -> std::string {
-    // fmt
-    using fmt::format;
-
     // std
     using std::stringstream;
 
@@ -83,7 +80,7 @@ inline auto render(
                 break;
             default:
                 if (!is_valid_var_char(c)) {
-                    throw setup_error(format(
+                    throw setup_error(fmt::format(
                         "Found invalid char '{}' in variable interpolation",
                         c));
                 }
@@ -104,7 +101,7 @@ inline auto render(
             default:
                 if (!is_valid_var_char(c)) {
                     throw setup_error(
-                        format("Found invalid char '{}' in variable name", c));
+                        fmt::format("Found invalid char '{}' in variable name", c));
                 }
                 var_buffer << c;
                 break;
@@ -120,7 +117,7 @@ inline auto render(
                 state = render_state::var_ending;
                 break;
             default:
-                throw setup_error(format(
+                throw setup_error(fmt::format(
                     "Found invalid char '{}' after variable name '{}'",
                     c,
                     var_buffer.str()));
@@ -141,7 +138,7 @@ inline auto render(
             }
             default:
                 throw setup_error(
-                    format("Found invalid char '{}' when expecting '}}'", c));
+                    fmt::format("Found invalid char '{}' when expecting '}}'", c));
             }
             break;
 
